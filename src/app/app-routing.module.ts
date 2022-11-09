7707d1787f7e9d4d619d4aaf61f38da8d7a1894f
Jonathan <yotomc17@gmail.com>
2022-11-09 01:03:44 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { PostulacionesComponent } from './postulaciones/postulaciones.component';
+import { VacantesComponent } from './vacantes/vacantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',pathMatch:'full', redirectTo:'inicio'
+  },
+  //Cualquier otra ruta va redireccionar a Inicio
+  {
+    path:'**', redirectTo:'inicio'
+  },
+  {
+    path:'inicio', component:InicioComponent
+  },
+  {
+    path:'admin', component:AdminComponent
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+  {
+    path:'postulaciones', component:PostulacionesComponent
+  },
+  {
+    path:'empresas', component:EmpresasComponent
+  },
+  {
+    path:'vacantes', component:VacantesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { VacantesComponent } from './vacantes/vacantes.component';
+import { PostulacionesComponent } from './postulaciones/postulaciones.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    VacantesComponent,
+    PostulacionesComponent,
+    EmpresasComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
