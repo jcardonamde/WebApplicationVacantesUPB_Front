14512a0dbf4b83d0769c234e9fdc9dcecad2d9c2
Jonathan <yotomc17@gmail.com>
2022-11-16 23:17:29 -0500

    create connection between backend and frontend. Testing public and local API

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  //Creo variable de tipo any por que desconocemos como viene la información o el tipo de dato
+  datos:any;
 
+  // Esto me permite generar consultas a los servicios
+  constructor(private http:HttpClient) { }
+
+  // Este método se ejecuta cuando la plantilla es creada
   ngOnInit(): void {
-  }
 
+    //Enviamos token de autenticación
+    const token = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJteUlkNDU3ODk2NjQxMzU1NSIsInN1YiI6IkpvbmF0aGFuQyIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2Njg2NTcwMjgsImV4cCI6MTY2ODY1NzYyOH0.rgsOj0rvJZXVK3uizqrQ3ZsEdjB2yqSlsQT4TxWj3CPiudSthOauVy_eHNqmOktj-9lQSMLmxgzzjFggiX4Mkg";
+
+    //Agregamos encabezado de autorización
+    const httpOptions ={
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+        Authorization:'Bearer '+token
+      })
+    };
+
+    // Definimos la ruta de la API que vamos a consumir
+    let url = "http://localhost:8089/api/empresas";
+    this.http.get(url, httpOptions)
+    // Guardamos los datos de la API en la variable de datos creada inicialmente
+    .subscribe(
+      // Función anonima
+      result => {
+        this.datos = result;
+      }
+    );
+  }
 }
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,23 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  //Creo variable de tipo any por que desconocemos como viene la info o el tipo de dato
-  datos: any;
+  constructor() { }
 
-  // Esto me permite generar consultas a los servicios
-  constructor(private http:HttpClient) { }
-
-  // Este método se crea cuando la plantilla es creada
   ngOnInit(): void {
-    let url = "https://api.fabricasoftware.co/?angular";
-    this.http.get(url)
-    // Guardamos los datos de la API en la variable de datos creada inicialmente
-    .subscribe(
-      // Función anonima
-      result => {
-        this.datos = result;
-      }
-    );
   }
-
 }
